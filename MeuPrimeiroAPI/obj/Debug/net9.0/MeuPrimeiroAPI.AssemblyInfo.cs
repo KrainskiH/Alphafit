af4ab659a53229ce5bb3d943f274b5318bac52e5
Krainski.H <henriquekrainski38@gmail.com>
2025-09-10 19:46:51 -0300

    arrumando

--- MeuPrimeiroAPI/obj/Debug/net9.0/MeuPrimeiroAPI.AssemblyInfo.cs
+++ MeuPrimeiroAPI/obj/Debug/net9.0/MeuPrimeiroAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MeuPrimeiroAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+482d39c1c0fa41b13c622c2e8c2d28994abf90f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5a6c30daad82f7157d4b72a146f3948ce2c0838")]
 [assembly: System.Reflection.AssemblyProductAttribute("MeuPrimeiroAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MeuPrimeiroAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
