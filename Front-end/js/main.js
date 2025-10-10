4623044556482e0d1eeadc94dbb6b26508ed91a6
Krainski.H <henriquekrainski38@gmail.com>
2025-10-10 20:11:26 -0300

    header novamente

--- Front-end/js/main.js
+++ Front-end/js/main.js
@@ -52,3 +52,12 @@ const titulo = document.getElementById("TITULO");
   }
 
   animar();
+
+  window.addEventListener("scroll", () => {
+  const header = document.querySelector("header");
+  if (window.scrollY > 50) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
